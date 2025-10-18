4a5e453fe8ae31e9cb215c03c470518e5d37b874
Elon Musk <you@example.com>
2025-10-18 16:08:50 +0000

    Modification de api.js

--- citil-frontend/src/services/api.js
+++ citil-frontend/src/services/api.js
@@ -239,7 +239,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8002';
+const API_URL = 'http://localhost:8000';
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
